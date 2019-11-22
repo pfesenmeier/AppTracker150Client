8f75a73a894a576bc2a5ec49de15f73697c02428
pfesenmeier@gmail.com <pfesenmeier@gmail.com>
2019-11-22 14:07:40 -0500

    appication to application

---
+++ AppTracker150Client/src/app/components/student/application/application-create/application-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-application-create',
+  templateUrl: './application-create.component.html',
+  styleUrls: ['./application-create.component.css']
+})
+export class ApplicationCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ AppTracker150Client/src/app/components/student/application/application-delete/application-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-application-delete',
+  templateUrl: './application-delete.component.html',
+  styleUrls: ['./application-delete.component.css']
+})
+export class ApplicationDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
